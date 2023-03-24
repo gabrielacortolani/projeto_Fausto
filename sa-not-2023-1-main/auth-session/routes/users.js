89813da5074a29d10ab8697b109011f1eae18c11
gabriela <cardoso.gabicardoso@gmail.com>
2023-03-23 22:30:23 -0300

    Aula23-03

--- sa-not-2023-1-main/auth-session/routes/users.js
+++ sa-not-2023-1-main/auth-session/routes/users.js
@@ -3,5 +3,9 @@ var router = express.Router();
 const controller = require('../controllers/users')
 
 router.get('/form', controller.formNew)
+router.get('/login', controller.formLogin)
+router.post('/', controller.create)
+router.post('/auth', controller.auth)
 
 module.exports = router;
+
